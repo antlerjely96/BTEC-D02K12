90099aa47f10a5013d4dbb4f8062a3a0a482cb5b
antlerjely96 <antlerjely96@gmail.com>
2022-02-18 22:12:46 +0700

    add BT buoi 13

---
+++ js/buoi13.js
@@ -0,0 +1,5 @@
+function demo(){
+    document.getElementById('the_p').innerHTML = "Đây không phải thẻ p";
+    document.getElementById('the_p').title = "Thẻ p";
+    document.getElementById('the_p').style.color = "red";
+}
